3f1d7fedb5a43a7d59095497c35aa58397e76325
Melissa Cabrera <melisac105@gmail.com>
2021-06-29 19:40:55 -0400

    Initial mockup

---
+++ src/main/java/ucf/assignments/ListWindowControllers.java
@@ -0,0 +1,44 @@
+package ucf.assignments;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class ListWindowControllers {
+    public TextField listName;
+    public TextField taskName;
+    public TextField taskDescription;
+
+    public void showAllButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void showCompletedItemsButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void showIncompletedItemsButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void downloadSingleListButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void backToMainWindowButtonClicked(MouseEvent mouseEvent) {
+    }
+
+    public void getListName(ActionEvent actionEvent) {
+    }
+
+    public void getTaskName(ActionEvent actionEvent) {
+    }
+
+    public void getTaskDescription(ActionEvent actionEvent) {
+    }
+
+    public void addItemButtonClicked(MouseEvent mouseEvent) {
+    }
+
+    public void doneButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void deleteItemButtonClicked(MouseEvent mouseEvent) {
+    }
+}
---
+++ src/main/java/ucf/assignments/MainWindowControllers.java
@@ -0,0 +1,24 @@
+package ucf.assignments;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class MainWindowControllers {
+    public TextField listName;
+
+    public void addListBottomClicked(MouseEvent mouseEvent) {
+    }
+
+    public void printListName(ActionEvent actionEvent) {
+    }
+
+    public void deleteListButtonClicked(MouseEvent mouseEvent) {
+    }
+
+    public void uploadListsButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void downloadAllListsButtonClicked(ActionEvent actionEvent) {
+    }
+}
